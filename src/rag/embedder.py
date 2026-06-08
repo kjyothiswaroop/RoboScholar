e8fb778bb831824adcd9aa764b414a1ff4f09291
Jyothi Swaroop <kjyothiswaroop00@gmail.com>
2026-06-08 17:10:56 -0500

    Added the Embedder part in RAG

---
+++ src/rag/embedder.py
@@ -0,0 +1,10 @@
+from sentence_transformers import SentenceTransformer
+
+class Embedder:
+
+    def __init__(self):
+        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    def embed(self, texts):
+        return self.model.encode(texts, show_progress_bar=False)
+    
